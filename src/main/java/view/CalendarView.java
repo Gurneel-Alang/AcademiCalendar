d7a9d2e93d09f44809443552f3cf80743ad43dbb
May <khue.truong@mail.utoronto.ca>
2026-07-26 22:16:35 -0400

    Added calendarpanel

--- src/main/java/view/CalendarView.java
+++ src/main/java/view/CalendarView.java
@@ -1,10 +1,24 @@
 package view;
 
 import java.swing.*;
+import com.github.lgooddatepicker.components.DatePickerSettings.DateArea;
 
 public class CalendarView {
     private final JFrame border = newJFrame();
-    private final JPanel calendarPanel;
 
-    
+    // Import calendar, and set coloring
+    DatePickerSettings calendarImport = new DatePickerSettings();
+    Color bgColor = new Color (180, 205, 205);
+    calendarImport.setColor(DateArea.BackgroundOverallCalendarPanel, bgColor);
+
+    // Set selected date as highlighted
+    calendarImport.sethighlightPolicy (new EventHighlightPolicy());
+
+    // Might need this for further events, delete if unnecessary
+    calendarPanel = new calendarPanel(dateSettings);
+    calendarPanel.setSelectedDate(LocalDate.now());
+
+    // Panel to hold the calendar
+    panel = new JPanel();
+    panel.add(calendarPanel, BorderLayout.NORTH)
 }
