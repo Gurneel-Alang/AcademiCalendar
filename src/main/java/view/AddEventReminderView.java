e90fb661a41a796032ded2c4f6eb5ffd8afa2400
May <khue.truong@mail.utoronto.ca>
2026-07-30 19:40:16 -0400

    Added test folder and few code snippet

---
+++ src/main/java/view/AddEventReminderView.java
@@ -0,0 +1,20 @@
+//package view;
+//
+//import javax.swing.BorderFactory;
+//import javax.swing.JButton;
+//import javax.swing.JLabel;
+//
+//import entity.event.Event;
+//
+//public class AddEventReminderView extends JPanel {
+//    private final JTextfield title = new JTextField (20);
+//
+//    private final AddEventDataAccessInterface eventDao;
+//
+//    public AddEventReminderView(
+//            AddEventReminderView eventDao) {
+//        this.eventDao = eventDao;
+//
+//    }
+
+}
